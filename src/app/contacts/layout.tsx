2277a11eed937ab5205fb51000a2004e9f6b17fa
Alexander Silich <sili41995@gmail.com>
2023-11-18 09:42:28 +0200

    update

--- src/app/contacts/layout.tsx
+++ src/app/contacts/layout.tsx
@@ -2,10 +2,8 @@ import SideBar from '@/components/SideBar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
-      <div className='w-full flex-none md:w-64'>
-        <SideBar />
-      </div>
+    <div className='container contacts-page'>
+      <SideBar />
       <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
     </div>
   );
--- src/components/NavLinks/NavLinks.tsx
+++ src/components/NavLinks/NavLinks.tsx
@@ -3,27 +3,28 @@
 import links from '@/constants/links';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import css from './NavLinks.module.css';
 
 export default function NavLinks() {
   const pathname = usePathname();
 
   return (
-    <>
+    <div className={css.container}>
       {links.map((link) => {
         // const LinkIcon = link.icon;
+        const isActiveLink = link.href === pathname && css.active;
+
         return (
           <Link
             key={link.name}
             href={link.href}
-            className={`${
-              link.href === pathname && 'bg-sky-100 text-blue-600'
-            } flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3`}
+            className={`${css.link} ${isActiveLink}`}
           >
             {/* <LinkIcon className='w-6' /> */}
-            <p className='hidden md:block'>{link.name}</p>
+            <p>{link.name}</p>
           </Link>
         );
       })}
-    </>
+    </div>
   );
 }
