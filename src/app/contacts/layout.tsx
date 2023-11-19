73f5304d0b695ba773856fefa2ce086fda1fb4ad
Alexander Silich <sili41995@gmail.com>
2023-11-19 10:35:28 +0200

    update

--- src/app/contacts/layout.tsx
+++ src/app/contacts/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='container contacts-page'>
       <SideBar />
-      <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
+      <div>{children}</div>
     </div>
   );
 }
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,28 +1,25 @@
 import Link from 'next/link';
 import NavLinks from '@/components/NavLinks';
 import css from './SideBar.module.css';
+import { signOut } from '../../../auth';
 
 export default function SideNav() {
   return (
     <div className={css.container}>
-      <Link
-        className='mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40'
-        href='/'
-      >
-        <div className='w-32 text-white md:w-40'>{/* <AcmeLogo /> */}</div>
+      <Link href='/'>
+        <div>{/* <AcmeLogo /> */}</div>
       </Link>
       <div className={css.linksContainer}>
         <NavLinks />
-        <div className='hidden h-auto w-full grow rounded-md bg-gray-50 md:block'></div>
         <form
-        // action={async () => {
-        //   'use server';
-        //   await signOut();
-        // }}
+          action={async () => {
+            'use server';
+            await signOut();
+          }}
         >
-          <button className='flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'>
+          <button className={css.signOutBtn}>
             {/* <PowerIcon className='w-6' /> */}
-            <div className='hidden md:block'>Sign Out</div>
+            <p>Sign Out</p>
           </button>
         </form>
       </div>
