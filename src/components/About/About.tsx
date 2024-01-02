c9cefa56c95636935388ebbd0e8163d8451b937a
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-01-02 13:13:34 +0200

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,12 +4,10 @@ import css from './About.module.css';
 const About: FC = () => (
   <p className={css.message}>
     This app was developed for demonstrate to showcase my skills in HTML, CSS,
-    JavaScript, TypeScript, React, React Router, and Redux Toolkit to future
+    JavaScript, TypeScript, React, Next.js, and Server Actions to future
     employers. A server application was developed to securely store information.
     This application was developed without the Axios to improve skills in
-    working with HTTP requests. Because under the hood of the Redux Toolkit is
-    Immer - we can freely &#34;mutate&#34; the state, and Immer will do it right
-    to keep us updated.
+    working with HTTP requests.
   </p>
 );
 
