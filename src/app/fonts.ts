59c673c74ef5b9497966f18e9c4f835f3bfd95bc
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-01-02 13:11:12 +0200

    update

---
+++ src/app/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter, Jua } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['500', '600'],
+  variable: '--font-inter',
+});
+
+export const jua = Jua({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+  variable: '--font-jua',
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import SharedLayout from '@/components/SharedLayout';
 import Toast from '@/components/Toast';
 import { auth } from '@/../auth';
 import { ILayoutProps } from '@/types/types';
+import { inter, jua } from '@/app/fonts';
 import './globals.css';
 
 const RootLayout: FC<ILayoutProps> = async ({ children }) => {
@@ -10,7 +11,7 @@ const RootLayout: FC<ILayoutProps> = async ({ children }) => {
   const isSignIn = Boolean(session);
 
   return (
-    <html>
+    <html className={`${inter.variable} ${jua.variable}`}>
       <body>
         <SharedLayout isSignIn={isSignIn}>{children}</SharedLayout>
         <Toast />
