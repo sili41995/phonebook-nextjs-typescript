39bb87ff5c04a54f2764c1b5ccf4512b6ec3f51a
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-01-03 09:20:48 +0200

    update

--- src/app/contacts/error.tsx
+++ src/app/contacts/error.tsx
@@ -4,8 +4,8 @@ import { FC } from 'react';
 import ErrorMessage from '@/components/ErrorMessage';
 import { IErrorProps } from '@/types/types';
 
-const Error: FC<IErrorProps> = ({ error, reset }) => (
-  <ErrorMessage reset={reset} message={error.message} />
+const Error: FC<IErrorProps> = ({ reset }) => (
+  <ErrorMessage reset={reset} message='Contact adding failed' />
 );
 
 export default Error;
