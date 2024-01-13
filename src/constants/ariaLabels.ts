da8bf2e022361aa475764b71323ac35d335b0942
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-01-13 21:04:24 +0200

    Create ariaLabels.ts

---
+++ src/constants/ariaLabels.ts
@@ -0,0 +1,17 @@
+const enum AriaLabels {
+  accept = 'accept',
+  cancel = 'cancel',
+  favorite = 'edit status "Favorite"',
+  delete = 'delete contact',
+  edit = 'edit contact',
+  authBtn = 'authentication button',
+  call = 'call button',
+  tgAccount = 'open a telegram account',
+  sendEmail = 'send email',
+  filter = 'change filter',
+  sort = 'change sort type',
+  pageBtn = 'go to page number',
+  avatar = 'attach an image',
+}
+
+export default AriaLabels;
